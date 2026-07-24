4c320ed62a6a9b9bcfba6934a16d8121a89b9bf8
Shawn Low <shawnlow10@gmail.com>
2026-07-24 13:40:20 +0800

    Fix swipe-back: nav tabs use replace to avoid building history stack

--- src/components/layout/BottomNavigation.tsx
+++ src/components/layout/BottomNavigation.tsx
@@ -220,6 +220,7 @@ export default function BottomNavigation() {
             <li key={item.to}>
               <NavLink
                 to={item.to}
+                replace
                 aria-label={item.label}
                 onClick={hapticLight}
                 className={({ isActive }) =>
